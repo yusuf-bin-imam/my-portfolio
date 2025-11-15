b45f7bdc3a7616026d2d57a76b9d4d6473b0ca35
Yusuf Bin Imam <108363841+yusuf-bin-imam@users.noreply.github.com>
2025-11-15 22:05:46 +0600

    update

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             id="text"
             className="text-xl sm:ml-4 sm:pl-4  sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
           >
-            Copyright © 2023 - All right reserved by yusufbinimam.
+            Copyright © 2025 - All right reserved by yusufbinimam.
           </p>
         </footer>
       </div>
